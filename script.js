eeb1398ccc94600220d3f8a28324cd52c6b4509e
fragius <189782341+Fragius@users.noreply.github.com>
2025-04-06 07:58:29 +0300

    Implement square colour changing

--- script.js
+++ script.js
@@ -8,8 +8,18 @@ function loadGrid(sideLength = 16) {
         for (let j = 0; j < sideLength; j++) { //j is the number of squares currently within the row
             const square = document.createElement("div");
             square.className = "square";
+            square.addEventListener("mouseenter", () => paintSquare(square))
             row.appendChild(square);
         }
         grid.appendChild(row);
     } 
 }
+
+function paintSquare(element) {
+    if (isColoured) {    
+        element.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    } else {
+        element.style.opacity = +element.style.opacity + 0.1;
+        console.log(element.style.opacity);
+    }
+}  
